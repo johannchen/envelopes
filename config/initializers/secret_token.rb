d9d82f141c74d5b9ccda57a9a592248df81735d3
Johann Chen <johannchen@gmail.com>
2011-12-08 16:32:06 -0800

    init rails 3.1.3 for envelops

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Envelops::Application.config.secret_token = 'b8f5bbde26da762c086f8164682a2e0874482ceef6ddf9e581184efb4c6c7d3c641e9ddd657eee1201d52863711ac033d1e4fbd927cc9329d8cde6503b38b005'
