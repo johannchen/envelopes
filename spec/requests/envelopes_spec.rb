d790b775276682f043c7ecd0cd67101eb45570cc
Johann Chen <johannchen@gmail.com>
2011-12-14 11:46:33 -0800

    spec envelopes

---
+++ spec/requests/envelopes_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "Envelopes" do
+  describe "Overview" do
+    it "displays monthly envelopes" do
+    end
+    it "displays annual/irregular envelopes" do
+    end
+    it "displays recent transactions" do
+    end
+  end
+
+  describe "Edit Budgets" do
+    it "edits monthly budget" do
+    end
+    it "edits annual budget" do
+    end
+    it "adds new envelope" do
+    end
+  end
+
+  describe "Distribution" do
+    it "allocates money to envelopes" do
+    end 
+  end
+
+  describe "Transer Between Envelopes" do
+    it "transfers money between two envelopes" do
+    end
+  end
+end
