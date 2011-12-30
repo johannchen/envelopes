f040e3891d44a10cdd93b8822d445ef1f05eac13
Johann Chen <johannchen@gmail.com>
2011-12-29 23:16:30 -0800

    expense breakdown report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,10 @@
 class ReportsController < ApplicationController
   def expense_breakdown
     @envelopes = Envelope.all
+    @total = 0
+    @envelopes.each do |e| 
+      @total += e.expense
+    end
   end
 
   def expense_vs_budget
