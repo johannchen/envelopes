c5dc750618b4aa7339fec1513d33b60eaa3d2fe2
Johann Chen <johannchen@gmail.com>
2012-01-29 18:14:38 -0800

    0.1.1

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,7 +5,7 @@ class ReportsController < ApplicationController
     authorize! :read, :reports
     params[:start_date] ? @start_date = params[:start_date] : @start_date = Date.today.at_beginning_of_month
     params[:end_date] ? @end_date = params[:end_date] : @end_date = Date.today.at_beginning_of_month.next_month
-    @envelopes = current_user.envelopes 
+    @envelopes = current_user.envelopes.order("name") 
     @total = @envelopes.inject(0.0) { |result, e| e.period_expense(@start_date, @end_date) + result }
     rows = @envelopes.map { |e| [e.name, e.period_expense(@start_date, @end_date)] } 
     @chart = pie_chart(rows, 'Envelope', 'Amount', "Expense Breakdown #{@start_date} to #{@end_date}") 
@@ -15,7 +15,7 @@ class ReportsController < ApplicationController
     authorize! :read, :reports
     params[:months] ? @months = params[:months].to_i : @months = 1 
     start_date = @months.months.ago.at_beginning_of_month.strftime("from %Y-%m-%d")
-    @envelopes = current_user.envelopes 
+    @envelopes = current_user.envelopes.order("name") 
     @total_expense = current_user.total_months_expense(@months)
     @total_budget = @envelopes.sum("budget") * @months
     @total_percentage = @total_expense / @total_budget * 100
@@ -25,7 +25,7 @@ class ReportsController < ApplicationController
 
   def budget_allocation
     authorize! :read, :reports
-    @envelopes = current_user.envelopes 
+    @envelopes = current_user.envelopes.order("name") 
     @total = current_user.total_budget_by_month
     rows = @envelopes.map { |e| e.monthly ? [e.name, e.budget.to_f] : [e.name, e.budget.to_f/12] } 
     @chart = pie_chart(rows, 'Envelope', 'Amount', 'Budget Allocation') 
