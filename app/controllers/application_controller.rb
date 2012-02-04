03d1a6ef25f830cb0c1d39fcf6d2cdc643b911f8
Johann Chen <johannchen@gmail.com>
2012-02-03 16:13:38 -0800

    setup for jquery mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::Base
   end
 
   check_authorization
+  
+  helper_method :current_user, :mobile_device?
+  before_filter :prepare_for_mobile
 
   private
 
@@ -13,5 +16,17 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else 
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
+    
 end
