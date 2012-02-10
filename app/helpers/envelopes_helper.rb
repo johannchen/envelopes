73fb1054f71dbb830e65c721796f8290f90a075e
Johann Chen <johannchen@gmail.com>
2012-02-10 15:41:07 -0800

    format progress bar

--- app/helpers/envelopes_helper.rb
+++ app/helpers/envelopes_helper.rb
@@ -1,2 +1,9 @@
 module EnvelopesHelper
+  def amount_percentage(envelope)
+    if envelope.budget == 0 or envelope.current_amount <= 0
+      "0.00%"
+    else
+      number_to_percentage(envelope.current_amount / envelope.budget * 100, :precision => 2)
+    end
+  end
 end
